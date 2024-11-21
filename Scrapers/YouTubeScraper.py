74765784e79d8f2e7bab3c7d5465fde3d846d8bd
shjiwon <hhllchtt@gmail.com>
2024-11-21 15:21:28 +0900

    youtubescraper ad

--- Scrapers/YouTubeScraper.py
+++ Scrapers/YouTubeScraper.py
@@ -14,15 +14,14 @@ class YouTubeScraper:
         self.youtube_crawler.open_page(channel_url)
         self.youtube_crawler.click_first_shorts()
 
-        captions_data = {}
+        captions_data = []
 
         for i in range(num_shorts):
+            if self.youtube_crawler.avoid_advertisement():
+                 self.youtube_crawler.click_next_shorts()
             current_url = self.youtube_crawler.get_current_url()
             captions = self.transcript_extractor.fetch_youtube_captions(current_url)
-            current_title = self.youtube_crawler.get_current_title()
-            captions_data[current_title] = captions
-            print(f"현재 URL: {current_url}")
-            print(f"현재 제목: {current_title}")
+            captions_data.append(captions)
             self.youtube_crawler.click_next_shorts()
 
         self.youtube_crawler.quit()
